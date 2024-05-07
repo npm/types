e16246906e1f6527fcda90fd28c2eb624977aa1e
Robert Kieffer <robert@broofa.com>
2024-05-07 03:55:13 -0700

    tsconfig tweaks

---
+++ test/fixtures/not-licensed@1.0.0.ts
@@ -0,0 +1,55 @@
+
+
+import type * as npmTypes from '../../types/index.d.ts';
+
+
+export const metadata: npmTypes.PackumentVersion = {
+  "name": "not-licensed",
+  "version": "1.0.0",
+  "description": "an empty package without license metadata",
+  "author": {
+    "name": "Kyle E. Mitchell",
+    "email": "kyle@kemitchell.com",
+    "url": "https://kemitchell.com/"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/jslicense/not-licensed.js.git"
+  },
+  "gitHead": "bd842fd7a3ac910f4c9cf084253bf99d2aab0a46",
+  "bugs": {
+    "url": "https://github.com/jslicense/not-licensed.js/issues"
+  },
+  "homepage": "https://github.com/jslicense/not-licensed.js#readme",
+  "_id": "not-licensed@1.0.0",
+  "scripts": {},
+  "_shasum": "e0c0d3022af1e51a49d24703c9c7a0c30a0f59a8",
+  "_from": ".",
+  "_npmVersion": "3.7.1",
+  "_nodeVersion": "5.5.0",
+  "_npmUser": {
+    "name": "kemitchell",
+    "email": "kyle@kemitchell.com"
+  },
+  "dist": {
+    "shasum": "e0c0d3022af1e51a49d24703c9c7a0c30a0f59a8",
+    "tarball": "https://registry.npmjs.org/not-licensed/-/not-licensed-1.0.0.tgz",
+    "integrity": "sha512-U9DmnjDE+hh2EGC4ug7XOKHlxVkbv2+vguUXeHT0ffGUChyt6LsX5MZ8q5cuH05el8BQrCrxzzBkrGnDuFwV0Q==",
+    "signatures": [
+      {
+        "keyid": "SHA256:jl3bwswu80PjjokCgh0o2w5c2U4LhQAE57gj9cz1kzA",
+        "sig": "MEUCICnUmc7y4OyUP8Cz5CFTnXBO3mzUTs+c5L9Fn7AjTml1AiEAk7VlYRz8GPdNGY+HgURUiX9ceiOLSZyMop2uBleTbOM="
+      }
+    ]
+  },
+  "maintainers": [
+    {
+      "name": "kemitchell",
+      "email": "kyle@kemitchell.com"
+    }
+  ],
+  "_npmOperationalInternal": {
+    "host": "packages-5-east.internal.npmjs.com",
+    "tmp": "tmp/not-licensed-1.0.0.tgz_1454738610221_0.42489640787243843"
+  }
+};
