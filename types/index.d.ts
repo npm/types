a2ea4c532ceefccd78eac122e594d7b421a7d05f
reggi <reggi@github.com>
2024-09-20 10:34:21 -0400

    flip usage

--- types/index.d.ts
+++ types/index.d.ts
@@ -86,18 +86,18 @@ interface Dist {
   unpackedSize?: number
 }
 
+interface DevEngineDependency {
+  name: string
+  version?: string
+  onFail?: 'ignore' | 'warn' | 'error' | 'download'
+}
+
 interface DevEngines {
   os?: DevEngineDependency | DevEngineDependency[]
   cpu?: DevEngineDependency | DevEngineDependency[]
   libc?: DevEngineDependency | DevEngineDependency[]
   runtime?: DevEngineDependency | DevEngineDependency[]
   packageManager?: DevEngineDependency | DevEngineDependency[]
-}
-
-interface DevEngineDependency {
-  name: string
-  version?: string
-  onFail?: 'ignore' | 'warn' | 'error' | 'download'
 }
 
 // this is in the tarball for the project. it really could have anything in it.
